08954c8a6e0709c2b64a250d5fabf90b1df9fc2b
Mehreen Butt <mbutt@republiccharterschools.org>
2018-05-31 19:10:58 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board,index)
   if board[index] != " "
     true
-  elsif board[index] ==" " || board[index] == nil
+  elsif board[index] ==" " || board[index] == "nil"
     false
   end
 end
